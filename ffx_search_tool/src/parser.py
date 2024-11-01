0c132c94bdb27b805276da64ea710ab1858f9802
andreasSchauer <andreas.schauer@yahoo.com>
2024-11-01 23:12:15 +0100

    B1: bribe table

--- ffx_search_tool/src/parser.py
+++ ffx_search_tool/src/parser.py
@@ -2,6 +2,8 @@ import json
 import importlib.resources
 from rich.console import Console
 from rich.table import Table
+from rich import box
+from rich.align import Align
 
 with importlib.resources.open_text("ffx_search_tool.data", "monsters.json") as file:
     monster_data = json.load(file)
@@ -10,22 +12,28 @@ with importlib.resources.open_text("ffx_search_tool.data", "monsters.json") as f
 console = Console()
 
 stat_names = ["HP (Overkill)", "MP", "Strength", "Defence", "Magic", "Magic Defence", "Agility", "Luck", "Evasion", "Accuracy"]
-item_names = ["Steal (Normal)", "Steal (Rare)", "Drop (Normal)", "Drop (Rare)"]
+item_names = ["Steal (Normal)", "Steal (Rare)", "Drop (Normal)", "Drop (Rare)", "Bribe"]
 
 
 # stats, dropped weapons/armor and items for defeating, stealing and bribing
 def monster_search(monster_name):
     monster = monster_data[monster_name]
-    stat_table = get_stat_table(monster)
-    item_table = get_item_table(monster)
+    table = Table(pad_edge=False, box=box.MINIMAL_HEAVY_HEAD, min_width=75)
+    
+    table.add_column(monster_name.title())
 
-    console.print(item_table)
+    table.add_row(get_stat_table(monster))
+    table.add_row(get_item_table(monster))
+    table.add_row(get_bribe_table(monster))
+
+    console.print(table)
 
 
 def get_stat_table(monster):
     stats = monster["stats"]
     stat_keys = list(stats.keys())
-    stat_table = Table(title="Stats", show_header=False)
+    stat_table = Table(show_lines=True, expand=True, box=box.SQUARE)
+    stat_table.add_column("Stats")
 
     for i in range(0, len(stat_keys), 2):
         left_key = stat_keys[i]
@@ -41,7 +49,6 @@ def get_stat_table(monster):
             right_val = str(stats[right_key])
         
         stat_table.add_row(left_stat, left_val, right_stat, right_val)
-        stat_table.add_section()
 
     return stat_table
 
@@ -49,35 +56,40 @@ def get_stat_table(monster):
 def get_item_table(monster):
     items = monster["items"]
     item_keys = list(items.keys())
-    item_table = Table(title="Items", show_header=False)
+    item_table = Table(show_lines=True, expand=True, box=box.SQUARE)
+    item_table.add_column("Items")
 
-    for i in range(0, len(item_keys), 2):
+    for i in range(0, len(item_keys), 1):
         left_name = item_names[i]
         left_key = item_keys[i]
         left_item = items[left_key][0].title()
         left_amt = items[left_key][1]
-        right_name = item_names[i+1]
-        right_key = item_keys[i+1]
-        right_item = items[right_key][0].title()
-        right_amt = items[right_key][1]
         
-        item_table.add_row(left_name, f"{left_item} x{left_amt}", right_name, f"{right_item} x{right_amt}")
-        item_table.add_section()
+        
+        item_table.add_row(left_name, f"{left_item} x{left_amt}")
+
+        
+
 
     return item_table
-    
-
-monster_search("varuna")
 
 
-"""
-    stats_table.add_row("HP (Overkill)", f"{stats["hp"][0]} ({stats["hp"][1]})", "MP", str(stats["mp"]))
-    stats_table.add_section()
-    stats_table.add_row("Strength", str(stats["strength"]), "Defence", str(stats["defence"]))
-    stats_table.add_section()
-    stats_table.add_row("Magic", str(stats["magic"]), "Magic Defence", str(stats["mag_defence"]))
-    stats_table.add_section()
-    stats_table.add_row("Agility", str(stats["agility"]), "Luck", str(stats["luck"]))
-    stats_table.add_section()
-    stats_table.add_row("Evasion", str(stats["evasion"]), "Accuracy", str(stats["accuracy"]))
-"""
+
+def get_bribe_table(monster):
+    hp = monster["stats"]["hp"][0]
+    hp_factors = [10, 15, 20, 25]
+    probabilities = [25, 50, 75, 100]
+
+    bribe_table = Table(show_lines=True, expand=True, box=box.SQUARE)
+    bribe_table.add_column("Bribe Success Rate")
+    bribe_table.add_row("Amount in Gil", "Success Rate")
+
+    for i in range(len(hp_factors)):
+        bribe_table.add_row(f"{hp * hp_factors[i]}", f"{probabilities[i]}%")
+
+    return bribe_table
+
+
+monster_search("water flan")
+
+
