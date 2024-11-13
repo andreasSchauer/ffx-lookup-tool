3cb73f4fa118e307a7e9839e9d5f4285a3d7e33e
andreasSchauer <andreas.schauer@yahoo.com>
2024-11-13 22:13:34 +0100

    F6 more data

--- ffx_search_tool/src/parser.py
+++ ffx_search_tool/src/parser.py
@@ -1,10 +1,15 @@
-from ffx_search_tool.src.constants import DUPLICATES, LOCATIONS
+from ffx_search_tool.src.constants import DUPLICATES, LOCATIONS, PHASES
 from ffx_search_tool.data.monster_data import monster_data
 from ffx_search_tool.src.search.monster_search import select_duplicate, get_monster_table
 from ffx_search_tool.src.search.location_search import select_location, get_local_monsters, get_location_table
 
 
 def monster_search(monster_name):
+    if monster_name in PHASES:
+        for phase in PHASES[monster_name]:
+            get_monster_table(phase)
+        return
+    
     if monster_name in DUPLICATES:
         monster_name = select_duplicate(monster_name)
 
@@ -12,7 +17,7 @@ def monster_search(monster_name):
         raise Exception("Monster not found.")
     
     monster = monster_data[monster_name]
-
+    
     if monster["has_allies"]:
         for ally in monster["allies"]:
             get_monster_table(ally)
@@ -39,4 +44,4 @@ def location_search(location_name):
 
 
 
-monster_search("biran ronso")
+monster_search("yu pagoda")
